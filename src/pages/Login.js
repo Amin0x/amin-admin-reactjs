50f488d813989a72bbee3462e61c7753d6c52d18
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-07 15:25:23 +0300

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,15 +34,29 @@ const Login = (props) => {
   }
 
   return (
-    <div className='login-warp'>
-      <div className='login-card'>
-        <div className='title'>login</div>
-        <input type='text' placeholder='USER NAME'/>
-        <input type='password' placeholder='PASSWORD' />        
-        <button onClick={() => login()}>Login</button>
-        <p><button className='forget-pass'>forget password?</button></p>
-      </div>
+    
+<div>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
     </div>
+    <form>
+        <h3>Login Here</h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username">
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password">
+
+        <button>Log In</button>
+        <div class="social">
+          <div class="go"><i class="fab fa-google"></i>  Google</div>
+          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div>
+    </form>
+</div>
+
   )
 }
 
