43a979297ae6902e202533142d935326f0242fdc
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-07 18:11:43 +0300

    Update TransactionDetails.js

--- src/pages/TransactionDetails.js
+++ src/pages/TransactionDetails.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function TransactionDetails() {
   return (
-    <div>TransactionDetails</div>
+    <div>
+<div class="card">
+  <div class="header"><h4><b>Header Title</b></h4></div>
+  <div class="container">
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div>
+</div>
+</div>
   )
 }
 
