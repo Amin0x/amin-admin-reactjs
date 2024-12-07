109ea724cb6ecdb1a8d71bff8805d3d1459bec79
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-07 13:44:33 +0300

    Update NavbarMenu.js

--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -1,7 +1,15 @@
-<ul>
-  <li>Lrm</li>
-  <li>Mollitia</li>
-  <li>Commod!</li>
-  <li>Dolorum?</li>
-  <li>Soluta.</li>
-</ul>
+import React from 'react';
+
+const NavbarMenu = () => {
+  return (
+    <ul>
+      <li>Lrm</li>
+      <li>Mollitia</li>
+      <li>Commod!</li>
+      <li>Dolorum?</li>
+      <li>Soluta.</li>
+    </ul>
+  );
+}
+
+export default NavbarMenu;
