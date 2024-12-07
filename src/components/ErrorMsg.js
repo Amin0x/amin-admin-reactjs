2434ebc9f66051f137a4dbf34e9578a86810eb8b
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-07 18:29:54 +0300

    Update ErrorMsg.js

--- src/components/ErrorMsg.js
+++ src/components/ErrorMsg.js
@@ -11,7 +11,13 @@ const ErrorMsg = (props) => {
    
         
         return (
-            <div>ErrorMsg</div>
+            <div class="callout">
+  <div class="callout-header">Callout Header</div>
+  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+  <div class="callout-container">
+    <p>Some text...</p>
+  </div>
+</div>
         )
     
 }
