9a567da43b0ead2aaa3b232807c284f97fbbd03c
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-07 13:46:25 +0300

    Update NavbarMenu.js

--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -3,11 +3,11 @@ import React from 'react';
 const NavbarMenu = () => {
   return (
     <ul>
-      <li>Lrm</li>
-      <li>Mollitia</li>
-      <li>Commod!</li>
-      <li>Dolorum?</li>
-      <li>Soluta.</li>
+      <li>Home</li>
+      <li>Categories</li>
+      <li>Users</li>
+      <li>Orders</li>
+      <li>Others</li>
     </ul>
   );
 }
