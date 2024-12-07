f6fa76e227b98efb3c8f1386f44ecd516d3be2e4
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-07 18:22:18 +0300

    Update components

---
+++ src/components/Alert.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const Alert = ({ message }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    isVisible && (
+      <div className="alert">
+        <span className="closebtn" onClick={handleClose}>
+          &times;
+        </span>
+        {message}
+      </div>
+    )
+  );
+};
+
+export default Alert;
