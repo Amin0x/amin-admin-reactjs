5716c8d3e5bcc011bd22f0cce606eaa9cdc7790b
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-09 23:55:08 +0300

    Update components

---
+++ src/components/SectionWrapper.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './SectionWrapper.css';
+
+export default function SectionWrapper ({ children, details, title }) {
+  return(
+    <div className="card">
+      <div className="card-details">
+        <h2>{title}</h2>
+        {details}
+      </div>
+      {children}
+    </div>
+  )
+}
+
+SectionWrapper.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element), 
+    PropTypes.element.isRequired
+  ]),
+  details: PropTypes.element,
+  title: PropTypes.string.isRequired,
+}
+
+SectionWrapper.defaultProps = {
+  details: null,
+}
