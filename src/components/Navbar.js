e709155bc3e6f37e6afbeea0bc8d0d010d9387aa
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-27 22:35:42 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,22 @@ import React from 'react'
 
 function Navbar(props) {
   return (
-    <div class="d-flex">
-    <div>
-    <a href="#"><img src="" alt=""></a>
-    <a href="#"><i class="fas fa-"></i></a>
-    <a href=""><i class="fas fa-"></i></a>
-    </div>
-      <div style={{minHeight:64, background: '#000'}}>Navbar</div>
+    <div className="d-flex justify-content-between" style={{ alignItems: 'center' }}>
+      <div>
+        <a href="#"><img src="" alt="Logo" /></a>
+      </div>
+      <div style={{ flex: 1, padding: '0 10px' }}>
+        <input 
+          type="text" 
+          placeholder="Search..." 
+          style={{ width: '100%', padding: '8px', borderRadius: '4px', border: '1px solid #ccc' }}
+        />
+      </div>
+      <div>
+        <a href="#"><i className="fas fa-icon1"></i></a>
+        <a href="#"><i className="fas fa-icon2"></i></a>
+      </div>
+      <div style={{ minHeight: 64, background: '#000', color: '#fff' }}>Navbar</div>
     </div>
   )
 }
