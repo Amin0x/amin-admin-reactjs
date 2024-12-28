5e27802149b16f07165f3ad607261861291f138f
Amin <49003095+Amin0x@users.noreply.github.com>
2024-12-28 14:06:25 +0300

    Update ErrorMsg.js

--- src/components/ErrorMsg.js
+++ src/components/ErrorMsg.js
@@ -2,23 +2,15 @@ import React, { Component } from 'react'
 
 const ErrorMsg = (props) => {
     
-      
-        this.state = {
-           
-        }
-    
-
-   
-        
-        return (
-            <div class="callout">
-  <div class="callout-header">Callout Header</div>
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-  <div class="callout-container">
-    <p>Some text...</p>
-  </div>
-</div>
-        )
+    return (
+        <div class="callout">
+          <div class="callout-header">Callout Header</div>
+          <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+          <div class="callout-container">
+            <p>Some text...</p>
+          </div>
+        </div>
+    );
     
 }
 
